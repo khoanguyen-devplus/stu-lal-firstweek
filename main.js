7c7e2fe0a6137cb8cd3eb27bd865b52523414c72
nhutnguyen2000 <nhut.nguyen@devplus.edu.vn>
2022-07-21 10:27:05 +0700

    create feauture preloader

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ const lightbox = GLightbox({
   touchNavigation: true,
   loop: true,
 });
+
+// Preloading
+let loader = document.getElementById("loader");
+setTimeout(function(){ loader.style.display = "none"; }, 1300);
